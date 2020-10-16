166aa41f5429f5eb48a29f3a9dba53ac8b83fb9e
Reza Aghajani <rezaaghajani@protonmail.com>
2020-10-16 13:50:06 +0330

    Learn about filesystems in NodeJs ✔

---
+++ Nodejs/files.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+// reading files 
+/* fs.readFile('./docs/blog.txt' , (err,data) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(data.toString())
+
+    console.log('last line')
+}) */
+
+// writing files
+
+/* fs.writeFile('./docs/blog.txt' , 'hello , world', () => {
+    console.log('file is written')
+}) */
+
+// directories
+/* if(!fs.existsSync('./assets')) {
+    fs.mkdir('./assets' , (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder created')
+    })
+} else {
+    fs.rmdir('./assets' , (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder deleted')
+    })
+} */

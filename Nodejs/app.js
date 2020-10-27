6f9abb24bfdc514cc51e1dd58743a66aaf24b8ae
Reza Aghajani <rezaaghajani@protonmail.com>
2020-10-28 03:09:59 +0330

    GoLang Hello World! ✔

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
-
-
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express()
+
+const dbURI = 'mongodb+srv://reza:1234@cluster0.dkk1h.mongodb.net/nodejs?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then((result) => app.listen(3000) )
+    .catch((err) => {console.log(err)})
 
 // register view engine
 app.set('view engine' , 'ejs')
@@ -15,7 +20,7 @@ app.use(express.static('public'))
 app.use(morgan('tiny'))
 
 app.get('/' , (req,res) => {
-    res.render('index')
+    res.redirect('/blogs')
 })
 
 app.get('/about' , (req,res) => {
@@ -24,6 +29,16 @@ app.get('/about' , (req,res) => {
 
 app.get('/blogs/create' , (req,res) => {
     res.render('create')
+})
+
+app.get('/blogs' , (req,res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render("blogs" , { title: 'Blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 

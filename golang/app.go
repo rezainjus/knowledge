881cdcc045620e368e82923a8c494ee987150116
Reza Aghajani <rezaaghajani@protonmail.com>
2020-11-07 19:19:46 +0330

    Learn about if/else in GoLang ✔

--- golang/app.go
+++ golang/app.go
@@ -5,12 +5,13 @@ import "fmt"
 // create a function that print every even numbers between 1 to 20
 
 func main() {
-	for n := 1; n < 20; n++ {
-		if n%2 == 0 {
-			fmt.Println(n)
-		} else { 
-			continue
-		}
+	num := 1
+	if num < 5 {
+		fmt.Println("increase the number")
+	} else if num == 9 {
+		fmt.Println("correct anwser")
+	} else {
+		fmt.Println("wrong!")
 	}
 
 }

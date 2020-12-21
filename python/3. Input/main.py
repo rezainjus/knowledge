adb668ba8ec5024318dc90b91f3c545403dfb5a4
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-21 13:12:46 +0330

    Get input from users in Python

---
+++ python/3. Input/main.py
@@ -0,0 +1,11 @@
+# Getting input from users
+inp = input()
+print(inp)
+
+# Getting input from users with text 
+inp1 = input("Your Question")
+print(inp1)
+
+# Getting input from users with text (more than one line)
+inp2 = input("Your \n Question")
+print(inp2)

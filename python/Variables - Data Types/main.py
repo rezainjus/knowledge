2d941c5a6c95d5009808460c70552431e17a11d9
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-21 13:05:50 +0330

    Learn Variables and Data Types in Python

---
+++ python/Variables - Data Types/main.py
@@ -0,0 +1,28 @@
+# Number count as int in python
+print(4)
+
+type(4) # output = int
+
+# ---------------
+
+#Text count as str in python
+print("Hello!")
+
+type("Hello") # output = str
+
+# ---------------
+
+# there is one more type of number that name float
+print(3.6)
+
+type(3.6) # output = float
+
+# ---------------
+
+# How to set a variable in python
+x = 3
+
+print(x) # output = 3
+
+# ---------------
+

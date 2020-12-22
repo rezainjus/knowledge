bbd9bea29de2b9761254d978a2edbe4dfd84da22
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-22 15:30:30 +0330

    Odd or Even Checker in Python ✔

---
+++ python/Training/OddOrEven.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+num = int(input("Enter your number: "))
+
+if num % 2 == 0:
+    print("Your number (" + str(num) + ") is Even.")
+elif num % 2 == 1:
+    print("Your number (" + str(num) + ") is Even.")
+else: 
+    print("Wrong Input!")

a83ba8b28fe75ae850500cdce26d92325276ee3b
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-23 15:01:32 +0330

    Learn try and except in Python ✔

--- python/12. Files/main.py
+++ python/12. Files/main.py
@@ -1,6 +1,17 @@
-file = open('text.txt') # Opening Files
+file = open('line.txt') # Opening Files
 
 print(file)
 
 filecontent = file.read()
 print(filecontent)
+
+fname = input("Enter your file name: ")
+
+try:
+    fhand = open(fname)
+except:
+    print("There is no file with that name" + fname)
+    exit()
+
+fread = fhand.read()
+print(fread)

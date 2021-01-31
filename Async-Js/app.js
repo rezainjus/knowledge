f648ff26c0d9d76922f94d1c90a4fd81f8af6e88
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-31 10:19:17 +0330

    Learn CallBack Functions

--- Async-Js/app.js
+++ Async-Js/app.js
@@ -1,11 +1,13 @@
-// Async code example
-
-
 
 console.log('Start')
 
-setTimeout(() => {
-    console.log('Timeout Test')
-},2000) 
+function userLogin(email,password,callback) {
+    setTimeout(() => {
+        console.log("Data Gathered!")
+        callback({userEmail: email})
+    },5000)
+}
+
+const user = userLogin('def@gmail.com',1234444,user => {console.log(user)})
 
 console.log('End')

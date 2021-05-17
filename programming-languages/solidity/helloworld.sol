60a6348fd1a8445796336955b9355558c169b60e
Reza Aghajani <rezaaghajani@protonmail.com>
2021-05-17 07:02:00 +0430

    Learn Constructor in Solidity

--- programming-languages/solidity/helloworld.sol
+++ programming-languages/solidity/helloworld.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.6.8;
 contract HelloWorld {
     string greetings;
     
+    constructor() public {
+        greetings = "Hello World!";
+    }
+    
     function get() public view returns(string memory) {
         return greetings;
     }
